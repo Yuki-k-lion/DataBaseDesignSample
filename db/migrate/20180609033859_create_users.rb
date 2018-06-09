8b00b8bec9a2a04a96fc89b9854dbc6bba6751b3
yuki komatsu <yuki.k.lion@gmail.com>
2018-06-09 12:43:36 +0900

    not null 制約のテスト

--- db/migrate/20180609033859_create_users.rb
+++ db/migrate/20180609033859_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-
+      #not null 制約の設定はここから行う
+      t.string :name, null: false
       t.timestamps
     end
   end
